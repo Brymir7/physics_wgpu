8f6c03d0322b31a993bad69eab57b86beedbdcbd
brymir <brymir7@gmail.com>
2023-04-03 13:31:22 +0200

    camera and cube vertices (not working yet)

--- src/window.rs
+++ src/window.rs
@@ -34,11 +34,223 @@ impl Vertex {
     }
 }
 
+// lib.rs
 const VERTICES: &[Vertex] = &[
-    Vertex { position: [0.0, 0.5, 0.0], color: [1.0, 0.0, 0.0] },
-    Vertex { position: [-0.5, -0.5, 0.0], color: [0.0, 1.0, 0.0] },
-    Vertex { position: [0.5, -0.5, 0.0], color: [0.0, 0.0, 1.0] },
+
 ];
+
+struct Cube {
+    position: [f32; 3],
+    size: f32,
+}
+impl Cube {
+    fn new(position: [f32; 3], size: f32) -> Self {
+        Self {
+            position,
+            size,
+        }
+    }
+    fn vertices(&self) -> [Vertex; 36] {
+        let x = self.position[0];
+        let y = self.position[1];
+        let z = self.position[2];
+        let size = self.size;
+
+        // Define the vertices for each face of the cube
+        let front_vertices = [  
+            Vertex { position: [x - size, y - size, z + size], color: [1.0, 0.0, 0.0] }, // A
+            Vertex { position: [x + size, y - size, z + size], color: [1.0, 0.0, 0.0] }, // B
+            Vertex { position: [x + size, y + size, z + size], color: [1.0, 0.0, 0.0] }, // C
+            Vertex { position: [x - size, y + size, z + size], color: [1.0, 0.0, 0.0] }, // D
+        ];
+        let back_vertices = [           
+            Vertex { position: [x - size, y - size, z - size], color: [0.0, 1.0, 0.0] }, // E
+            Vertex { position: [x - size, y + size, z - size], color: [0.0, 1.0, 0.0] }, // F
+            Vertex { position: [x + size, y + size, z - size], color: [0.0, 1.0, 0.0] }, // G
+            Vertex { position: [x + size, y - size, z - size], color: [0.0, 1.0, 0.0] }, // H
+        ];
+        let top_vertices = [            
+            Vertex { position: [x - size, y + size, z + size], color: [0.0, 0.0, 1.0] }, // D
+            Vertex { position: [x + size, y + size, z + size], color: [0.0, 0.0, 1.0] }, // C
+            Vertex { position: [x + size, y + size, z - size], color: [0.0, 0.0, 1.0] }, // G
+            Vertex { position: [x - size, y + size, z - size], color: [0.0, 0.0, 1.0] }, // F
+        ];
+        let bottom_vertices = [            
+            Vertex { position: [x - size, y - size, z + size], color: [1.0, 1.0, 0.0] }, // A
+            Vertex { position: [x - size, y - size, z - size], color: [1.0, 1.0, 0.0] }, // E
+            Vertex { position: [x + size, y - size, z - size], color: [1.0, 1.0, 0.0] }, // H
+            Vertex { position: [x + size, y - size, z + size], color: [1.0, 1.0, 0.0] }, // B
+        ];
+        let left_vertices = [
+            Vertex { position: [x - size, y - size, z + size], color: [0.0, 1.0, 1.0] }, // A
+            Vertex { position: [x - size, y + size, z + size], color: [0.0, 1.0, 1.0] }, // D
+            Vertex { position: [x - size, y + size, z - size], color: [0.0, 1.0, 1.0] }, // F
+            Vertex { position: [x - size, y - size, z - size], color: [0.0, 1.0, 1.0] }, // E
+        ];
+        let right_vertices = [
+            Vertex { position: [x + size, y - size, z + size], color: [0.0, 1.0, 1.0] }, // B
+            Vertex { position: [x + size, y + size, z + size], color: [0.0, 1.0, 1.0] }, // C
+            Vertex { position: [x + size, y + size, z - size], color: [0.0, 1.0, 1.0] }, // G
+            Vertex { position: [x + size, y - size, z - size], color: [0.0, 1.0, 1.0] }, // H
+        ];
+        let mut vertices = [Vertex{position: [0.0, 0.0, 0.0], color: [0.0, 0.0, 0.0]}; 36];
+        let faces = [&front_vertices, &back_vertices, &top_vertices, &bottom_vertices, &left_vertices, &right_vertices];
+        let mut idx = 0;
+    for face_vertices in &faces {
+        vertices[idx] = face_vertices[0]; idx += 1;
+        vertices[idx] = face_vertices[1]; idx += 1;
+        vertices[idx] = face_vertices[2]; idx += 1;
+
+        vertices[idx] = face_vertices[2]; idx += 1;
+        vertices[idx] = face_vertices[3]; idx += 1;
+        vertices[idx] = face_vertices[0]; idx += 1;
+    }
+        vertices
+    }
+}
+//indices enable to reuse data of vertex (triangles that use the same vertices for example)
+const INDICES: &[u16] = &[
+    0, 1, 4,
+    1, 2, 4,
+    2, 3, 4,
+];
+
+#[rustfmt::skip]
+pub const OPENGL_TO_WGPU_MATRIX: cgmath::Matrix4<f32> = cgmath::Matrix4::new(
+    1.0, 0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0, 0.0,
+    0.0, 0.0, 0.5, 0.0,
+    0.0, 0.0, 0.5, 1.0,
+);
+struct CameraController {
+    speed: f32,
+    is_forward_pressed: bool,
+    is_backward_pressed: bool,
+    is_left_pressed: bool,
+    is_right_pressed: bool,
+}
+
+impl CameraController {
+    fn new(speed: f32) -> Self {
+        Self {
+            speed,
+            is_forward_pressed: false,
+            is_backward_pressed: false,
+            is_left_pressed: false,
+            is_right_pressed: false,
+        }
+    }
+
+    fn process_events(&mut self, event: &WindowEvent) -> bool {
+        match event {
+            WindowEvent::KeyboardInput {
+                input: KeyboardInput {
+                    state,
+                    virtual_keycode: Some(keycode),
+                    ..
+                },
+                ..
+            } => {
+                let is_pressed = *state == ElementState::Pressed;
+                match keycode {
+                    VirtualKeyCode::W | VirtualKeyCode::Up => {
+                        self.is_forward_pressed = is_pressed;
+                        true
+                    }
+                    VirtualKeyCode::A | VirtualKeyCode::Left => {
+                        self.is_left_pressed = is_pressed;
+                        true
+                    }
+                    VirtualKeyCode::S | VirtualKeyCode::Down => {
+                        self.is_backward_pressed = is_pressed;
+                        true
+                    }
+                    VirtualKeyCode::D | VirtualKeyCode::Right => {
+                        self.is_right_pressed = is_pressed;
+                        true
+                    }
+                    _ => false,
+                }
+            }
+            _ => false,
+        }
+    }
+
+    fn update_camera(&self, camera: &mut Camera) {
+        use cgmath::InnerSpace;
+        let forward = camera.target - camera.eye;
+        let forward_norm = forward.normalize();
+        let forward_mag = forward.magnitude();
+
+        // Prevents glitching when camera gets too close to the
+        // center of the scene.
+        if self.is_forward_pressed && forward_mag > self.speed {
+            camera.eye += forward_norm * self.speed;
+        }
+        if self.is_backward_pressed {
+            camera.eye -= forward_norm * self.speed;
+        }
+
+        let right = forward_norm.cross(camera.up);
+
+        // Redo radius calc in case the fowrard/backward is pressed.
+        let forward = camera.target - camera.eye;
+        let forward_mag = forward.magnitude();
+
+        if self.is_right_pressed {
+            // Rescale the distance between the target and eye so 
+            // that it doesn't change. The eye therefore still 
+            // lies on the circle made by the target and eye.
+            camera.eye = camera.target - (forward + right * self.speed).normalize() * forward_mag;
+        }
+        if self.is_left_pressed {
+            camera.eye = camera.target - (forward - right * self.speed).normalize() * forward_mag;
+        }
+    }
+}
+
+struct Camera {
+    eye: cgmath::Point3<f32>,
+    target: cgmath::Point3<f32>,
+    up: cgmath::Vector3<f32>,
+    aspect: f32,
+    fovy: f32,
+    znear: f32,
+    zfar: f32,
+}
+impl Camera {
+    fn build_view_projection_matrix(&self) -> cgmath::Matrix4<f32> {
+        // 1.
+        let view = cgmath::Matrix4::look_at_rh(self.eye, self.target, self.up);
+        // 2.
+        let proj = cgmath::perspective(cgmath::Deg(self.fovy), self.aspect, self.znear, self.zfar);
+
+        // 3.
+        return OPENGL_TO_WGPU_MATRIX * proj * view;
+    }
+}
+#[repr(C)]
+// This is so we can store this in a buffer
+#[derive(Debug, Copy, Clone, bytemuck::Pod, bytemuck::Zeroable)]
+struct CameraUniform {
+    // We can't use cgmath with bytemuck directly so we'll have
+    // to convert the Matrix4 into a 4x4 f32 array
+    view_proj: [[f32; 4]; 4],
+}
+
+impl CameraUniform {
+    fn new() -> Self {
+        use cgmath::SquareMatrix;
+        Self {
+            view_proj: cgmath::Matrix4::identity().into(),
+        }
+    }
+
+    fn update_view_proj(&mut self, camera: &Camera) {
+        self.view_proj = camera.build_view_projection_matrix().into();
+    }
+}
+
 struct State {
     surface: wgpu::Surface,
     device: wgpu::Device,
@@ -46,9 +258,15 @@ struct State {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     window: Window,
+    camera: Camera,
+    camera_uniform: CameraUniform,
+    camera_buffer: wgpu::Buffer,
+    camera_controller: CameraController,
+    camera_bind_group: wgpu::BindGroup,
     render_pipeline: wgpu::RenderPipeline,
     vertex_buffer: wgpu::Buffer,
-    num_vertices : u32,
+    index_buffer: wgpu::Buffer,
+    num_indices: u32,
 }
 
 impl State {
@@ -117,18 +335,65 @@ impl State {
             view_formats: vec![],
         };
         surface.configure(&device, &config);
+        let camera = Camera {
+            eye: (0.0, 1.0, 2.0).into(),
+            target: (0.0, 0.0, 0.0).into(),
+            up: cgmath::Vector3::unit_y(),
+            aspect: config.width as f32 / config.height as f32,
+            fovy: 45.0,
+            znear: 0.1,
+            zfar: 100.0,
+        };
 
-        let shader = device.create_shader_module(wgpu::ShaderModuleDescriptor {
-            label: Some("Shader"),
-            source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
+        let mut camera_uniform = CameraUniform::new();
+        camera_uniform.update_view_proj(&camera);
+
+        let camera_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Camera Buffer"),
+                contents: bytemuck::cast_slice(&[camera_uniform]),
+                usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+            }
+        );
+        let camera_bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: None,
+                    },
+                    count: None,
+                }
+            ],
+            label: Some("camera_bind_group_layout"),
         });
+        let camera_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            layout: &camera_bind_group_layout,
+            entries: &[
+                wgpu::BindGroupEntry {
+                    binding: 0,
+                    resource: camera_buffer.as_entire_binding(),
+                }
+            ],
+            label: Some("camera_bind_group"),
+        });
+        let camera_controller = CameraController::new(0.05);
+        
         let shader = device.create_shader_module(wgpu::include_wgsl!("shader.wgsl"));
-        let render_pipeline_layout =
-            device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        let render_pipeline_layout = device.create_pipeline_layout(
+            &wgpu::PipelineLayoutDescriptor {
                 label: Some("Render Pipeline Layout"),
-                bind_group_layouts: &[],
+                bind_group_layouts: &[
+                    &camera_bind_group_layout,
+                ],
                 push_constant_ranges: &[],
-            });
+            }
+        );
+        
+        
         let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
             label: Some("Render Pipeline"),
             layout: Some(&render_pipeline_layout),
@@ -170,14 +435,22 @@ impl State {
             },
             multiview: None, // 5.
         });
+        let cube = Cube::new([0.0, 0.0, 0.0], 1.0);
         let vertex_buffer = device.create_buffer_init(
             &wgpu::util::BufferInitDescriptor {
                 label: Some("Vertex Buffer"),
-                contents: bytemuck::cast_slice(VERTICES),
+                contents: bytemuck::cast_slice(&cube.vertices()),
                 usage: wgpu::BufferUsages::VERTEX,
             }
         );
-        let num_vertices = VERTICES.len() as u32;
+        let index_buffer = device.create_buffer_init(
+            &wgpu::util::BufferInitDescriptor {
+                label: Some("Index Buffer"),
+                contents: bytemuck::cast_slice(INDICES),
+                usage: wgpu::BufferUsages::INDEX,
+            }
+        );
+        let num_indices = INDICES.len() as u32;
         Self {
             surface,
             device,
@@ -185,9 +458,15 @@ impl State {
             config,
             size,
             window,
+            camera,
+            camera_uniform,
+            camera_buffer,
+            camera_bind_group,
+            camera_controller,
             render_pipeline,
             vertex_buffer,
-            num_vertices,
+            index_buffer,
+            num_indices,
         }
     }
 
@@ -206,44 +485,14 @@ impl State {
 
     #[allow(unused_variables)]
     fn input(&mut self, event: &WindowEvent) -> bool {
-        match event {
-            WindowEvent::MouseInput { button, state, .. } => {
-                match button {
-                    MouseButton::Left => {
-                        // handle left mouse button input
-                        match state {
-                            ElementState::Pressed => {
-                                // handle left mouse button press
-                                println!("Left mouse button pressed!");
-                            }
-                            ElementState::Released => {
-                                // handle left mouse button release
-                                println!("Left mouse button released!");
-                            }
-                        }
-                    }
-                    MouseButton::Right => {
-                        // handle right mouse button input
-                        match state {
-                            ElementState::Pressed => {
-                                // handle right mouse button press
-                                println!("Right mouse button pressed!");
-                            }
-                            ElementState::Released => {
-                                // handle right mouse button release
-                                println!("Right mouse button released!");
-                            }
-                        }
-                    }
-                    _ => {}
-                }
-                true // consume the event
-            }
-            _ => false, // do not consume the event
-        }
+        self.camera_controller.process_events(event)
     }
 
-    fn update(&mut self) {}
+    fn update(&mut self) {
+        self.camera_controller.update_camera(&mut self.camera);
+        self.camera_uniform.update_view_proj(&self.camera);
+        self.queue.write_buffer(&self.camera_buffer, 0, bytemuck::cast_slice(&[self.camera_uniform]));
+    }
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         let output = self.surface.get_current_texture()?;
@@ -276,8 +525,11 @@ impl State {
                 depth_stencil_attachment: None,
             });
             render_pass.set_pipeline(&self.render_pipeline); // 2.
+            // NEW!
+            render_pass.set_bind_group(0, &self.camera_bind_group, &[]);
             render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
-            render_pass.draw(0..self.num_vertices, 0..1); // 3.
+            render_pass.set_index_buffer(self.index_buffer.slice(..), wgpu::IndexFormat::Uint16);
+            render_pass.draw_indexed(0..self.num_indices, 0, 0..1);
         }
         self.queue.submit(iter::once(encoder.finish()));
         output.present();
